2e43299d47b7b6253f0cf12760bfab9c820df5a8
Flanman20 <76894031+Flanman20@users.noreply.github.com>
2025-04-12 18:36:23 -0400

    Version 0.2.3.2

--- Assets/Scripts/Extras/Metadata.cs
+++ Assets/Scripts/Extras/Metadata.cs
@@ -2,6 +2,6 @@ using System;
 
 public static class Metadata
 {
-    public const string VERSION = "0.2";
+    public const string VERSION = "0.2.3.1";
     public const string BUILD_NUMBER = "01.25";
 }
